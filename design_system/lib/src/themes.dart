c71cc2ed45b6d1f666509ea2aca97cd2ed7ca8f4
PauloVOFonseca <paulovitoradsifba@gmai.com>
2022-08-10 12:33:59 -0300

    first commit with themes

---
+++ design_system/lib/src/themes.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+final themeLight = ThemeData(primarySwatch: Colors.deepPurple);
+
+final themeDark = ThemeData(primarySwatch: Colors.deepPurple, brightness: Brightness.dark);
